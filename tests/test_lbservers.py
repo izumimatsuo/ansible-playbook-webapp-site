173b6cfe3d2e6a2ef08f86fa0384fbd0f7f0f33b
いずみ <izumi@Matsuo-MacBook-Air.local>
2021-06-14 00:41:51 +0900

    update tests

--- tests/test_lbservers.py
+++ tests/test_lbservers.py
@@ -10,6 +10,18 @@ def test_running_default_service(host):
     assert host.service('haproxy').is_running
     assert host.service('keepalived').is_running
 
+
+# VIPが設定されている
+def test_keepalived_state_and_vip(host):
+    state = host.run("journalctl -u keepalived |grep STATE |tail -n 1")
+    addresses = host.interface("eth1").addresses
+
+    if 'MASTER' in state.stdout:
+        assert '192.168.33.10' in addresses
+    else:
+        assert '192.168.33.10' not in addresses
+
+
 # 公開サービスが意図したポートでリッスンしている
 def test_listen_default_port(host):
     assert host.socket('tcp://0.0.0.0:443').is_listening
