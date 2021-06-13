c5a1cdc615162fb9ad9cbb27c0647a604c442ac8
いずみ <izumi@Matsuo-MacBook-Air.local>
2021-06-14 01:57:36 +0900

    update tests

--- tests/test_appservers.py
+++ tests/test_appservers.py
@@ -8,6 +8,13 @@ def test_installed_default_package(host):
 def test_running_default_service(host):
     assert host.service('docker').is_running
 
+
+# Swarm クラスタが起動している
+def test_swarm_active(host):
+    state = host.run("docker info | grep Swarm:")
+    assert 'Swarm: active' in state.stdout
+
+
 # 公開サービスが意図したポートでリッスンしている
 # def test_listen_default_port(host):
 #     assert host.socket('tcp://0.0.0.0:80').is_listening
